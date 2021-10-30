650d2bc70562d43a0dae927c74a5645570ed0464
Dmitry <pprefera@gmail.com>
2021-10-30 22:28:01 +0300

    call button

--- Web-begin/Diplom_1/6_1 flex 2/scripts/mob.js
+++ Web-begin/Diplom_1/6_1 flex 2/scripts/mob.js
@@ -4,3 +4,24 @@ $(function(){
 		menu.slideToggle(500);
   	})
 });
+$(document).ready(
+	function () {
+  console.log("document loaded");
+  $(function () {
+    //2. Получить элемент, к которому необходимо добавить маску
+    $(".phone-class").mask("8(999) 999-99-99");
+  });
+  // modal window for phone call
+  //open window
+$(function(){
+	//modal conntainer
+	//var ModalPhoneCallContainer = $('modal-phone-call-container');
+	//var CallButton = $('call-button');
+	//var ModalPhoneCallClose = $('modal-phone-call-close');
+	//events
+	$('call-button').click(function () {
+		$('modal-phone-call-container').css('display','block');	
+	})
+		
+
+});
