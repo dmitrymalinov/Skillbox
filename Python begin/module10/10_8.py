cd9d7dab43e667b85c4944c5affbda0bbdd63542
Dmitry <pprefera@gmail.com>
2023-08-03 19:24:51 +0300

    sole 10_8

--- Python begin/module10/10_8.py
+++ Python begin/module10/10_8.py
@@ -1,6 +1,7 @@
 print('Задача 8. Яма ')
 
-# Представьте, что вы разрабатываете компьютерную игру с текстовой графикой. Вам поручили создать генератор ландшафта. Напишите программу, которая получает на вход число N и выводит на экран числа в виде ямы:
+# Представьте, что вы разрабатываете компьютерную игру с текстовой графикой. 
+# Вам поручили создать генератор ландшафта. Напишите программу, которая получает на вход число N и выводит на экран числа в виде ямы:
 
 # Напишите программу,
 # которая получает на вход число N и выводит на экран числа в виде “ямы”:
@@ -12,3 +13,18 @@ print('Задача 8. Яма ')
 # 543....345
 # 5432..2345
 # 5432112345
+pit_height = int(input("Введите высоту ямы: "))
+current_dots = 0
+for every_row in range(pit_height):
+    for every_column in range(every_row + 1):
+        current_symbol_to_print = pit_height - every_column
+        print(current_symbol_to_print,end='')
+    current_dots = pit_height - (every_column + 1)
+    print('.' * current_dots,end = '')
+    for every_column in range(1,pit_height+1):
+        if every_column <= pit_height - (every_row+1):
+            print('.',end = '')
+        else:
+            print(every_column,end='') 
+    print()
+   
