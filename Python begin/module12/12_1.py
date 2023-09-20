d8432cd0f17c99ba03eb4de3a8674f2e6515258a
dmitrymalinov <pprefera@gmail.com>
2023-09-20 14:54:38 +0300

    12_1 problem solve

--- Python begin/module12/12_1.py
+++ Python begin/module12/12_1.py
@@ -8,3 +8,10 @@ print('Задача 1. Сумма чисел')
 # Введите число: 5
 # 
 # Я знаю, что сумма чисел от 1 до 5 равна 15
+def summa_n(number):
+    summ_number = 0
+    for current_number in range(1,number + 1):
+        summ_number += current_number
+    print(f"Я знаю, что сумма чисел от 1 до {number} равна {summ_number}")
+input_number = int(input("Введите число: "))
+summa_n(input_number)
