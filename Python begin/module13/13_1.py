7ff216c6091932ab35736617e9e7644cfbbbbeb1
dmitrymalinov <pprefera@gmail.com>
2023-10-20 16:23:56 +0300

    13_1 solve

--- Python begin/module13/13_1.py
+++ Python begin/module13/13_1.py
@@ -1,3 +1,4 @@
+import math
 print('Задача 1. Урок информатики 2')
 
 
@@ -22,3 +23,29 @@ print('Задача 1. Урок информатики 2')
 # 
 # Введите число: 0.0012
 # Формат плавающей точки: x = 1.2 * 10 ** -3
+# Если у нас число маленькое и степень чилса минусовая то нам надо выйти к 1
+#иначе к 0 в делении с остатком
+def float_format(number,direction):
+    current_number = 0
+    while True:
+        last_digit = number // math.pow(10,direction)
+        ss=1
+        if direction < 0:
+            if last_digit == 1:
+                print(f"Формат плавающей точки: x = {number / math.pow(10,direction)} * 10 ** {direction}")
+                break
+            direction -= 1
+        else:
+            if 1 <= last_digit < 10:
+                print(f"Формат плавающей точки: x = {number / math.pow(10,direction)} * 10 ** {direction}")
+                break   
+            direction += 1    
+    
+number = float(input("Введите число: "))
+ss=1
+current_direction = number // 10
+choose_direction = 0
+if current_direction == 0:
+    float_format(number,-1)
+else:
+    float_format(number,1)
