8caa2b245c92c3087e98c7d851e3b29dda1d071c
dmitrymalinov <pprefera@gmail.com>
2023-10-23 15:58:56 +0300

    13_6 solve

--- Python begin/module13/13_6.py
+++ Python begin/module13/13_6.py
@@ -1,3 +1,4 @@
+import math
 print('Задача 6. Яйца')
 
 # В рамках программы колонизации Марса
@@ -29,3 +30,40 @@ print('Задача 6. Яйца')
 # Введите максимально допустимый уровень опасности: 0.01
 # 
 # Приблизительная глубина безопасной кладки: 0.732421875 м
+MIN_DEEP = 0
+MAX_DEEP = 4
+
+def danger_level(deep_meters):
+    current_danger_level = math.pow(deep_meters,3) - 3*math.pow(deep_meters,2) - 12*deep_meters + 10
+    return current_danger_level
+max_danger_level = float(input("Введите максимально допустимый уровень опасности: "))
+#идем от 4 не доходя до 0 на каждой итерации уменьшая глубину в половину
+previuous_deep = 0
+current_deep = MAX_DEEP
+left = 0
+rigth = 0
+flag = 0
+while True:
+    current_danger_lvl = danger_level(current_deep)
+    if round(current_danger_lvl,2) == max_danger_level:# наш ответ
+        print(f"Приблизительная глубина безопасной кладки: {right} м")
+        break
+    if round(current_danger_lvl,2) > max_danger_level: # перелет
+        if flag == 1:
+            left = current_deep
+            current_deep = (left + right)/2
+            continue
+        flag = 1
+        left = current_deep
+        right = previuous_deep
+        previuous_deep = current_deep
+        current_deep = (left + right)/2
+        
+        continue
+    if round(current_danger_lvl,2) < max_danger_level: # недолет
+        if flag == 1:
+            right = current_deep
+            current_deep = (left + right)/2
+            continue
+        previuous_deep = current_deep
+        current_deep /=2
